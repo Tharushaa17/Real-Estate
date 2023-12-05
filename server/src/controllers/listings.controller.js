5bff106f5a886eafa394c903e3680abc1c2841f7
tharushaa17 <tharushaa.td17@gmail.com>
2023-12-05 21:57:27 +0530

    delete list

--- server/src/controllers/listings.controller.js
+++ server/src/controllers/listings.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listings.model.js"
+import { errorHandler } from "../utils/errorhandler.js";
 
 export const createListing = async (req, res, next) => {
     try {     
@@ -6,5 +7,23 @@ export const createListing = async (req, res, next) => {
         return res.status(200).json(listing);
     } catch (error) {
         next(error);
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(401, "Listing not found!"));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, "You can only delete your account"))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+    } catch (error) {
+        next(error)
     }
 }
